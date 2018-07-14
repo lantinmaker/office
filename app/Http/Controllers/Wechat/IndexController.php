3039a550d0dae08172a032bbbc8f47dedf7487dd
domaker <maker68@163.com>
2018-07-14 13:20:55 +0800

    修复验证机制

--- app/Http/Controllers/Wechat/IndexController.php
+++ app/Http/Controllers/Wechat/IndexController.php
@@ -7,26 +7,36 @@ use App\Http\Controllers\Controller;
 
 class IndexController extends Controller
 {
-    const TOKEN = "LantinMaker";
+    const TOKEN = "lantinmaker";
 
     public function __construct()
     {
 
     }
 
-    public function index(Request $request)
+    public function checkSignature($signature , $timestamp , $nonce)
     {
-        $signature = $request->get('signature');
-        $timestamp = $request->get('timestamp');
-        $nonce = $request->get('nonce');
-
         $tmpArr = array(self::TOKEN , $timestamp, $nonce);
         sort($tmpArr, SORT_STRING);
         $tmpStr = implode( $tmpArr );
         $tmpStr = sha1( $tmpStr );
 
         if( $signature  === $tmpStr){
-            return 1;
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    public function index(Request $request)
+    {
+
+        $signature = $request->get('signature');
+        $timestamp = $request->get('timestamp');
+        $nonce = $request->get('nonce');
+
+        if($this->checkSignature($signature , $timestamp , $nonce)){
+            return $request->get('echostr');
         }else{
             return 0;
         }
